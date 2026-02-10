f96a7456505168e8a2176d7ea255b3fc038c9a91
Priyal Babel <priyal.babel@somaiya.edu>
2026-02-09 17:53:26 -0800

    11 Container with most water

---
+++ 11_Container_with_most_water/11_Container_with_most_water.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_vol = 0
+        for i in range(len(heights)):
+            for j in range(i+1, len(heights)):
+                vol = (j-i) * min(heights[i],heights[j])
+                if vol > max_vol:
+                    max_vol = vol
+        return max_vol
